c687c0e8b7fb54137ac8da55170e043d4bbf320f
Yandy Ramirez <yandyr@gmail.com>
2026-02-19 18:36:37 +0000

    fix: keep delay profile defaults in minutes

--- ops/5.add-default-delay-profile-with-temporary-settings.sql
+++ ops/5.add-default-delay-profile-with-temporary-settings.sql
@@ -5,5 +5,5 @@
 -- @opIds: 66
 
 -- --- BEGIN op 66 ( create delay_profile "Prefer Usenet" )
-insert into "delay_profiles" ("name", "preferred_protocol", "usenet_delay", "torrent_delay", "bypass_if_highest_quality", "bypass_if_above_custom_format_score", "minimum_custom_format_score") values ('Prefer Usenet', 'prefer_usenet', 300, 0, 0, 0, NULL);
+insert into "delay_profiles" ("name", "preferred_protocol", "usenet_delay", "torrent_delay", "bypass_if_highest_quality", "bypass_if_above_custom_format_score", "minimum_custom_format_score") values ('Prefer Usenet', 'prefer_usenet', 5, 10, 0, 0, NULL);
 -- --- END op 66
